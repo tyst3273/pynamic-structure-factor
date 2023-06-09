861efbc70c26ba03914b90fd41b8e8715260a7d8
tyst3273 <ty.sterling@colorado.edu>
2023-06-09 16:39:28 -0600

    asdasd

---
+++ PSF_api.py
@@ -0,0 +1,42 @@
+#!/home/ty/anaconda3/bin/python3
+
+#   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#   !                                                                           !
+#   ! Copyright 2021 by Tyler C. Sterling and Dmitry Reznik,                    !
+#   ! University of Colorado Boulder                                            !
+#   !                                                                           !
+#   ! This file is part of the pynamic-structure-factor (PSF) software.         !
+#   ! PSF is free software: you can redistribute it and/or modify it under      !
+#   ! the terms of the GNU General Public License as published by the           !
+#   ! Free software Foundation, either version 3 of the License, or             !
+#   ! (at your option) any later version. PSF is distributed in the hope        !
+#   ! that it will be useful, but WITHOUT ANY WARRANTY; without even the        !
+#   ! implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  !
+#   ! See the GNU General Public License for more details.                      !
+#   !                                                                           !
+#   ! A copy of the GNU General Public License should be available              !
+#   ! alongside this source in a file named gpl-3.0.txt. If not see             !
+#   ! <http://www.gnu.org/licenses/>.                                           !
+#   !                                                                           !
+#   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+"""
+perform calculation using 'API'. can pass args in manually
+"""
+
+# custom modules
+from psf.m_PSF import c_PSF
+
+# --------------------------------------------------------------------------------------------------
+
+input_file = 'input_params.py'
+PSF = c_PSF(input_file)
+
+#PSF = c_PSF() # can optionally not use input file. 
+
+PSF.setup_calculation()
+PSF.run()
+
+# --------------------------------------------------------------------------------------------------
+
+
